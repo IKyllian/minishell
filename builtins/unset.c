22cbc227cd31734c1306e73ea7c9b4a4f57f23cd
Chafik Taleb <taleb.cas@gmail.com>
2021-10-30 10:16:26 +0200

    minor fixes

--- builtins/unset.c
+++ builtins/unset.c
@@ -3,14 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kdelport <kdelport@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ctaleb <ctaleb@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 09:59:23 by kdelport          #+#    #+#             */
-/*   Updated: 2021/10/30 07:59:55 by kdelport         ###   ########.fr       */
+/*   Updated: 2021/10/30 10:14:14 by ctaleb           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+int	is_valid_value(char c, int indx)
+{
+	if (((c >= 48 && c <= 57) || c == '=' || c == '+') && indx == 0)
+		return (0);
+	if ((c >= 48 && c <= 57) || (c >= 65 && c <= 90)
+		|| (c >= 97 && c <= 122) || (c == '_'))
+			return (1);
+	return (0);
+}
+
+int	check_value(char *value)
+{
+	int	i;
+
+	i = 0;
+	while (value[i])
+	{
+		if (!is_valid_value(value[i], i))
+		{
+			ft_putstr_fd("minishell: unset: ", 2);
+			ft_putstr_fd(value, 2);
+			ft_putstr_fd(": not a valid identifier\n", 2);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
 
 int	ft_unset(t_shell *shell, t_pars **cmd_parsed)
 {
@@ -18,6 +47,8 @@ int	ft_unset(t_shell *shell, t_pars **cmd_parsed)
 		(*cmd_parsed) = (*cmd_parsed)->next;
 	while ((*cmd_parsed) && (*cmd_parsed)->type == 2)
 	{
+		if (!check_value((*cmd_parsed)->value))
+			return (1);
 		srch_and_dlt_env_var(&shell->env, (*cmd_parsed)->value);
 		if (ft_strcmp("PWD", (*cmd_parsed)->value) == 0)
 			shell->cmd.set_old_to_null = 1;
--- parsing/validator.c
+++ parsing/validator.c
@@ -6,7 +6,7 @@
 /*   By: ctaleb <ctaleb@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 12:57:48 by ctaleb            #+#    #+#             */
-/*   Updated: 2021/10/29 10:28:26 by ctaleb           ###   ########lyon.fr   */
+/*   Updated: 2021/10/30 10:12:39 by ctaleb           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,29 @@ void	sendset_error(t_cmd *cmd)
 	ft_putstr_fd("minishell: syntax error ne", cmd->fd_out);
 	ft_putstr_fd("ar unexpected token '|'\n", cmd->fd_out);
 	cmd->exit_status = 258;
+}
+
+int	type_check(t_shell *shell)
+{
+	t_pars	*lst;
+	int		i;
+
+	lst = shell->cmd.parsed;
+	i = 1;
+	while (lst)
+	{
+		if (lst->type == 3)
+		{
+			i = 0;
+			lst = lst->next;
+			continue ;
+		}
+		if (lst->type == 5 && i > 1)
+			lst->type = 2;
+		i++;
+		lst = lst->next;
+	}
+	return (1);
 }
 
 int	validator(t_shell *shell)
@@ -45,5 +68,5 @@ int	validator(t_shell *shell)
 		}
 		lst = lst->next;
 	}
-	return (1);
+	return (type_check(shell));
 }
